6c61d839fb20ced1509d48acd142362760f9a1ef
szdytom <szdytom@163.com>
2020-01-26 23:40:33 +0800

    Continue to a hello

--- segmentoj/settings.py
+++ segmentoj/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'segmentoj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR+'/webui'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
