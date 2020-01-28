419e042fb3bb8f80ecae0798431caf109ac67511
szdytom <szdytom@163.com>
2020-01-28 21:56:39 +0800

    add problem list

--- segmentoj/urls.py
+++ segmentoj/urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.welcome),
+    path('problems/', views.problemlist),
 ]
--- segmentoj/views.py
+++ segmentoj/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from problem.models import Problem
 
 def welcome(request):
 	context = {}
 	return render(request, 'welcome.html', context)
+
+def problemlist(request):
+	context = {}
+	problemlist = Problem.objects.order_by('show_id')
+	context['problems'] = problemlist
+	return render(request, 'problemlist.html', context)
