868af0520cf65ee75134ed0d5414654a0f04676b
szdytom <szdytom@163.com>
2020-02-15 20:37:45 +0800

    init new app:soj

--- segmentoj/settings.py
+++ segmentoj/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # My Apps
+    # SegmentOJ Apps
     'problem',
+    'soj',
 ]
 
 MIDDLEWARE = [
---
+++ soj/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SojConfig(AppConfig):
+    name = 'soj'
