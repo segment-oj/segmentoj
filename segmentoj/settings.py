a60d9a08807a5901437ebe994ba9c75f7d9d0ce7
szdytom <szdytom@163.com>
2020-06-12 20:02:48 +0800

    install api

--- segmentoj/settings.py
+++ segmentoj/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+	# API
+	'rest_framework',
+	'api', # Segment OJ API
+
     # SegmentOJ Apps
     'problem',
     'soj',
