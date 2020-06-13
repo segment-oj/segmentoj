ac5b7fef69cd911dd63bd0400d99a24efffc0aca
szdytom <szdytom@163.com>
2020-06-13 10:33:12 +0800

    add new api for login & logout

--- api/urls.py
+++ api/urls.py
@@ -13,32 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from . import views
+from api import views
 
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.welcome),
-
-    # problem
-    path('problem/', include('problem.urls')),
-
-    # user
-    path('user/login', views.login),
-    path('user/logout', views.logout),
-    path('user/register', views.register),
-    path('user/home/<int:uid>', views.show_user),
-
-    # captcha
-    path('captcha/', include('captcha.urls')),
-
-    # api
-    path('api/', include('api.urls')),
+	path('user', views.UserView.as_view()),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+from django.views.decorators.csrf import csrf_exempt,csrf_protect
+from django.utils.decorators import method_decorator
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+import json
 
 # Create your views here.
+
+class UserView(APIView):
+
+	@method_decorator(csrf_exempt)
+	def post(self, request):
+		# Create user session(Login)
+		data = request.data
+		username = data['username']
+		password = data['password']
+
+		user = auth.authenticate(request,username=username,password=password)
+
+		if user:
+			auth.login(request, user)
+			return Response({"msg": "done."}, status=status.HTTP_201_CREATED)
+		else:
+			return Response({"msg": "Username or password wrong"}, status=status.HTTP_403_FORBIDDEN)
+
+	@method_decorator(csrf_exempt)
+	def delete(self, request):
+		# delete session(logout)
+
+		if not request.user.is_authenticated:
+			return Response({"msg": "Not logged in!"}, status=status.HTTP_401_UNAUTHORIZED)
+		
+		auth.logout(request)
+		return Response({"msg": "Success!"}, status=status.HTTP_205_RESET_CONTENT)
+	
+
