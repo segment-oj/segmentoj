74698e2286e797720ba4071de81cf7c7315acbb1
szdytom <szdytom@163.com>
2020-07-21 16:27:45 +0800

    fix syllable_required for return HTTP400

--- segmentoj/decorator.py
+++ segmentoj/decorator.py
@@ -15,7 +15,7 @@ def syllable_required(syllable_id, syllable_type=None, is_get_request=False):
             if syllable_type != None and type(r) != syllable_type:
                 return Response({
                     'detail': 'syllable {sid} has wrong type'.format(sid=syllable_id)
-                })
+                }, status=status.HTTP_400_BAD_REQUEST)
 
             return func(request, *args, **kwargs)
         return _wrapped_view
