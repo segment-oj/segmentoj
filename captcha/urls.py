905d62b82745e36132bbf2cee7f293f8ab711a9d
szdytom <szdytom@163.com>
2020-07-24 07:55:38 +0800

    remove unused imports

--- captcha/urls.py
+++ captcha/urls.py
@@ -13,12 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 import captcha.views
-
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path("get/<int:key>", captcha.views.getcaptcha),
