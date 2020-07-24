85d383a6e4cb5d1c53ea8a166ceb8cb9ccbc620e
szdytom <szdytom@163.com>
2020-07-24 19:55:43 +0800

    add view

--- judger/views.py
+++ judger/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from status import JudgeStatus as js
+from status.models import Status
+
 # Create your views here.
+class JudgerStatusView(APIView):
+    def get(self, request):
+        task_filter = {"state": js.JUDGE_STATUS_WAITING}
+        queryset = Status.objects.filter(**task_filter).order_by("id")[:1]
+        res_status = queryset[0]
+        res_status.state = js.JUDGE_STATUS_INPROCESS
+
+        return Response({
+            "res": res_status.id
+        }, status=status.HTTP_200_OK)
