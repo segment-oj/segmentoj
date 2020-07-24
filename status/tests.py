d8dc1ee695a948eaff90826cb392d2da33b4ed84
szdytom <szdytom@163.com>
2020-07-24 08:28:42 +0800

    add test case

--- status/tests.py
+++ status/tests.py
@@ -6,6 +6,9 @@ from rest_framework.test import APIRequestFactory, force_authenticate
 from .views import StatusView
 from account.models import User
 
+import status.JudgeLanguage as jl
+import status.JudgeStatus as js
+
 # Create your tests here.
 class StatusTest(TestCase):
     fixtures = ["testdatabase.yaml"]
@@ -30,3 +33,42 @@ class StatusTest(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
 
+    def testB_get_status(self):
+        ac_data = {
+            "id": 1,
+            "score": 100,
+            "state": js.JUDGE_STATUS_AC,
+            "lang": jl.JUDGE_LANGUAGE_CPP,
+            "code": "# Code",
+            "problem": 1,
+            "owner": 2,
+        }
+
+        request = self.factory.get(self.base_url, {"id": 1}, format="json")
+        res = self.view(request)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        data = res.data["res"]
+
+        self.assertEqual(data.get("id"), ac_data["id"])
+        self.assertEqual(data.get("score"), ac_data["score"])
+        self.assertEqual(data.get("state"), ac_data["state"])
+        self.assertEqual(data.get("lang"), ac_data["lang"])
+        self.assertEqual(data.get("code"), ac_data["code"])
+        self.assertEqual(data.get("problem"), ac_data["problem"])
+        self.assertEqual(data.get("owner"), ac_data["owner"])
+
+    # Try to submit problem without logged in
+    def testC_submit_problem_not_login(self):
+        request_data = {
+            "problem": 1,
+            "code": "# This is the code.",
+        }
+
+        user = User.objects.get(username="testuser")
+
+        request = self.factory.post(self.base_url, request_data, format="json")
+        force_authenticate(request)  # Clear Session
+        res = self.view(request)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
