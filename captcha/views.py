6d7b6d374d6fc187d834413d31c8643b41f81681
szdytom <szdytom@163.com>
2020-07-29 18:30:28 +0800

    fix view for captcha

--- captcha/views.py
+++ captcha/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import redirect
 
-from . import captcha
+from .captchas import setcaptcha
 
 # Create your views here.
 def getcaptcha(request, key):
-	captcha.setcaptcha(key)
+	setcaptcha(key)
 	return redirect("/media/captcha/{name}.png".format(name=key))
