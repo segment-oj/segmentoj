710fc8d435cb27819bfe177940f16134043b1f06
szdytom <szdytom@163.com>
2020-08-13 09:10:28 +0800

    update User by remove unused feilds

--- account/models.py
+++ account/models.py
@@ -3,14 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    username = models.CharField(max_length=128, unique=True)
-    USERNAME_FIELD = "username"
-    email = models.EmailField()
-
     introduction = models.TextField(blank=True, null=True, default="# Hello World!")
     lang = models.IntegerField(default=0)
     solved = models.IntegerField(default=0)
     submit_time = models.IntegerField(default=0)
+
+    first_name = models.NOT_PROVIDED # delete unused feild
+    last_name = models.NOT_PROVIDED # delete unused feild
 
     is_judger = models.BooleanField(default=False)
 
