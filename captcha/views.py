bad10a3c0b9450a6e092bfb2054a7ab4ed9d5114
szdytom <szdytom@163.com>
2020-09-11 21:27:49 +0800

    fix security #1

--- captcha/views.py
+++ captcha/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import redirect
+from rest_framework.response import Response
+from rest_framework import status
 
 from .captchas import setcaptcha
 
 # Create your views here.
 def getcaptcha(request, key):
+	if not key.isdigit():
+		return Response({
+			"detail": "Only digit is allow in captcha key"
+		}, status=status.HTTP_400_BAD_REQUEST)
+
 	setcaptcha(key)
 	return redirect("/media/captcha/{name}.png".format(name=key))
