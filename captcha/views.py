8e25e1356b0f6c851b1d4f622c3078a1c8d01523
zhangtianli2006 <zhangtianli2006@163.com>
2020-09-12 08:27:34 +0800

    format code
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- captcha/views.py
+++ captcha/views.py
@@ -5,11 +5,12 @@ from rest_framework import status
 from .captchas import setcaptcha
 
 # Create your views here.
-def getcaptcha(request, key):
-	if not str(key).isdigit():
-		return Response({
-			"detail": "Only digit is allow in captcha key"
-		}, status=status.HTTP_400_BAD_REQUEST)
 
-	setcaptcha(key)
-	return redirect("/media/captcha/{name}.png".format(name=key))
+def getcaptcha(request, key):
+    if not str(key).isdigit():
+        return Response({
+            "detail": "Only digit is allow in captcha key"
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    setcaptcha(key)
+    return redirect("/media/captcha/{name}.png".format(name=key))
