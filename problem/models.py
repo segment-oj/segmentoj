6c3a7f684e09bcade563d322b9c2b8583e46373e
szdytom <szdytom@163.com>
2021-02-03 16:23:02 +0800

    [feature] add field testdata_url in model problem

--- problem/models.py
+++ problem/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import indexes
+from django.db.models.base import Model
 
 # Create your models here.
 class Tag(models.Model):
@@ -9,11 +11,13 @@ class Tag(models.Model):
         return self.content
 
 class Problem(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-
     pid = models.IntegerField(unique=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
     date_added = models.DateTimeField(auto_now_add=True)
+    last_edit = models.DateTimeField(auto_now=True)
+    
     allow_html = models.BooleanField(default=False)
     enabled = models.BooleanField(default=True)
     tags = models.ManyToManyField(Tag, blank=True)
@@ -21,13 +25,17 @@ class Problem(models.Model):
     time_limit = models.IntegerField(default=1000)
     memory_limit = models.IntegerField(default=128000)
 
+    testdata_url = models.TextField(blank=True, null=True)
+
     class Meta:
         permissions = ( # (permission name, promission explanation)
             ('view_hidden', 'Can view hidden problem'),
         )
 
-    def getTags(self):
-        return self.tags.all()
+        indexes = [
+            models.Index(fields=['pid']),
+            models.Index(fields=['title']),
+        ]
 
     def __str__(self):
         return self.title
